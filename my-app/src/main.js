781617bdb7aed277bde338e013e7c60e53918590
Jenine Dulay <158535998+jenenerz@users.noreply.github.com>
2025-03-01 13:57:14 +0000

    testing

--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,6 +1,12 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import main from './components/main.vue'
+
+
+const app = createApp(App)
+
+app.component('main', main)
+
+
+app.mount('#app')
