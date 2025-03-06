b54159a5fe53640a4f0ea672a943fe4e083b13c9
Jenine Dulay <158535998+jenenerz@users.noreply.github.com>
2025-03-06 04:09:58 +0000

    updated

--- my-app/src/components/comment.vue
+++ my-app/src/components/comment.vue
@@ -231,7 +231,7 @@ import { ref, onMounted } from 'vue';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://jmfmnoxehtzhzvofgglp.supabase.co";
-const supabaseAnonKey = "YOUR_SUPABASE_ANON_KEY";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptZm1ub3hlaHR6aHp2b2ZnZ2xwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4MTUzMjYsImV4cCI6MjA1NjM5MTMyNn0.6Yl0_kpA3Ghoh2bDeEVccuVAr1QgiZ12SpYTei_K_MQ";
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const guestName = ref('');
